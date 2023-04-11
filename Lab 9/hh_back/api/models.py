1c47e2b8d32350418445f2dfcfec124de420dc8e
Aldiar Saparov <saparovaldiar1@gmail.com>
2023-04-11 23:05:41 +0600

    created api and company class

---
+++ Lab 9/hh_back/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    city = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
