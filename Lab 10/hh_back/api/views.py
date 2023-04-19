d3a07a3b69f453d36b5aed3b500085ed91624a15
Aldiar Saparov <saparovaldiar1@gmail.com>
2023-04-19 14:14:06 +0600

    added serializers and func views

--- Lab 10/hh_back/api/views.py
+++ Lab 10/hh_back/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.db.models import Q
+from .serializers import VacancySer, VacancyModSer
+from .models import Vacancy
 
 # Create your views here.
+
+def vacancy(request, id):
+    try:
+        vacancy = Vacancy.objects.get(id=id).to_json()
+        return JsonResponse(vacancy)
+    except Vacancy.DoesNotExist:
+        return JsonResponse({"error": "Vacancy not found!"})
+    
+def vacancies(request):
+    vacancies_obj = Vacancy.objects.all()
+    vacancies = VacancySer(vacancies_obj, many=True)
+
+    return JsonResponse(vacancies.data, safe=True)
+
+def get_top_ten_vacancies(request):
+    top_vacancies_obj = Vacancy.objects.filter(~Q(salary=None)).order_by('-salary')[:10]
+    top_vacancies = VacancyModSer(top_vacancies_obj, many=True)
+    
+    return JsonResponse(top_vacancies.data, safe=False)
+
